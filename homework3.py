74abab71555445f62390250befdecb1b78369dc1
Ruot-Folz <nonsonsmagic@gmail.com>
2024-07-15 19:53:54 +0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Dima'
+print ('Name:', name)
+age = 22
+print('Age:',age, type(age))
+new_age = age+2
+print('New Age:',new_age, type(age))
+is_student = 'True'
+print('Is Student:', is_student)
