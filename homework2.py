12e471d6135997853f7caf2e08f231b9a5e259ef
Ruot-Folz <nonsonsmagic@gmail.com>
2024-07-15 18:51:33 +0700

    homework2.py

--- homework2.py
+++ homework2.py
@@ -1,11 +1,11 @@
 first_name = 'Python'
 tasks = 12
-time = 0.125
-orange_count = 1.5
+result = 'tasks'/'time'
+time = 1.5
 print(first_name,
       ': всего задач:',
       tasks,
       ', затрачено часов:',
-      orange_count,
+      time,
       'среднее время выполнения:',
-      time)
+      result)
