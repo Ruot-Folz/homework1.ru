15a5f4a1ce9c8686e8a5c1270385c0cc6669ad64
Ruot-Folz <nonsonsmagic@gmail.com>
2024-07-16 15:31:34 +0700

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,5 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students_list = sorted(students)
+average_grades = {student: sum(grades[i]) / len(grades[i]) for i, student in enumerate(students_list)}
+print(average_grades)
