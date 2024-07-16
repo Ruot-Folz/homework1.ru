f597359d3c9a7ba80f51f702b9f17ce5b71922af
Ruot-Folz <nonsonsmagic@gmail.com>
2024-07-16 21:36:22 +0700

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -3,6 +3,6 @@ print(immutable_var)
 #immutable_var[0] = 10
 print(immutable_var)
 print('Кортеж не поддерживает обращение по элементам, кортеж больше подходит для хранения неизменяемого списка')
-mutable_list = ([5, 6], 7)
+mutable_list = [[5, 6], 7]
 mutable_list[0][1] = 9
 print(mutable_list)
